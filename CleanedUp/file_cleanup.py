57e89348c9f46570cb4f3b0e6f6f8eddf534897a
forgisonajeep <parker_c_18@yahoo.com>
2025-09-26 17:58:40 -0400

    Add Python exercises (CLEANEDUP)

---
+++ CleanedUp/file_cleanup.py
@@ -0,0 +1,17 @@
+
+import os
+
+folder_original = '/Users/killa/Documents/GitHub/luit-september-2025'
+folder_destination = '/Users/killa/Documents/GitHub/luit-september-2025/CleanedUp/'
+
+os.mkdir(folder_destination)
+
+entries = os.scandir(folder_original)
+
+for entry in os.scandir(folder_original):
+
+    location_original = os.path.join(folder_original, entry.name)
+    location_destination = os.path.join(folder_destination, entry.name)
+
+    if os.path.isfile(location_original):
+        os.rename(location_original, location_destination)
--- file_cleanup.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-folder = '/Users/killa/Documents/GitHub/luit-september-2025'
-
-entries = os.scandir(folder)
-
-for entry in entries:
-    print(entry.name)
